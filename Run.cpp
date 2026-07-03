3b6dd3403da0f114fd6705c5a89a5ee8d8972d26
hmj2622029Kadomoto <hmj2622029@stu.o-hara.ac.jp>
2026-07-03 16:52:46 +0900

    画像の追加、背景の移動の変更

--- Run.cpp
+++ Run.cpp
@@ -1,5 +1,6 @@
 #include "DxLib.h"
 #include "Run.h"
+#include <string.h>
 
 // 定数の定義
 const int WIDTH = 1200, HEIGHT = 720; // ウィンドウの幅と高さのピクセル数
@@ -10,18 +11,24 @@ const int IMG_GOODFOOD_TYPE = 7; // 良い食べ物の種類
 const int GOODFOOD_MAX = 1000; // 良い食べ物の最大数
 const int ATTACK_MAX = 1000;
 const int STAGE_DISTANCE = FPS * 60; // ステージの長さ
-enum { ONION, CHOCOLATE, GRAPE, COFFEE, BEER, AVOCADO, RAW_FISH, MILK, DOG_FOOD }; // ダメな食べ物の種類
-enum { COOKED_FISH, PASTE_VEGETABLES, APPLE, WATERMELON, BANANA, CAT_FOOD, CAT_MILK }; // 良い食べ物の種類
+enum { ONION, CHOCOLATE, GRAPE, COFFEE, BEER, AVOCADO, RAW_FISH, MILK, DOG_FOOD,	BADFOOD_KINDNUM }; // ダメな食べ物の種類
+enum { COOKED_FISH, VEGETABLES, APPLE, WATERMELON, BANANA, CAT_FOOD, CAT_MILK,	GOODFOOD_KINDNUM }; // 良い食べ物の種類
 enum { TITLE, PLAY, OVER, CLEAR }; // シーンを分けるための列挙定数
-
+const char BadFoodImageName[BADFOOD_KINDNUM][10] = { "Onion","Chocolate","Grape","Coffee","Beer","Avocado","RawFish","Milk","DogFood" };
+const char GoodFoodImageName[GOODFOOD_KINDNUM][13] = { "WaterMelon","Apple","Banana","Carrot","CatFood","CatMilk","CookedFish" };
 // グローバル変数
 struct OBJECT player; // 自機用の構造体変数 
+struct OBJECT attack[ATTACK_MAX]; // 攻撃用の構造体変数
+
 
 // ここでゲームに用いる変数や配列を定義する
-int imgStage;
+int imgGround;
+int imgSky;
+int imgMountain;
 int imgCat; // 自機の画像
-int imgBAD[BADFOOD_MAX]; // 敵機の画像
-int imgGOOD[GOODFOOD_MAX]; // アイテムの画像
+int imgClaw;
+int imgBAD[BADFOOD_KINDNUM]; // 敵機の画像
+int imgGOOD[GOODFOOD_KINDNUM]; // アイテムの画像
 //int bgm, jinOver, jinClear, seExpl, seItem, seShot; // 音の読み込み用
 int distance = 0; // ステージ終端までの距離
 //int bossIdx = 0;
@@ -29,7 +36,6 @@ int distance = 0; // ステージ終端までの距離
 int score = 0; // スコア
 int hisco = 10000; // ハイスコア
 int noDamageFrame = 0; // 無敵状態
-int weaponLv = 1; // 自機の武器のレベル（同時に発射される弾数）
 int scene = TITLE; // シーンを管理
 int timer = 0; // 時間の進行を管理
 
@@ -51,9 +57,10 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 
 		// ゲームの骨組みプログラムをここに記入する
 
-		ScrollBG(5);
+		ScrollBG(1);
 
 		MovePlayer();
+		MoveAttack();
 		ScreenFlip(); // 裏画面を表画面に反映させる
 		WaitTimer(1000 / FPS); // 一定時間待つ
 		if (ProcessMessage() == -1) { break; } // Windowsから情報を受け取りエラーが起きたら終了
@@ -67,22 +74,32 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
 void InitGame(void) // Initは初期化という意味（initialize）
 {
 	// 背景用の画像の読み込み
-	imgStage = LoadGraphWithCheck("repo用素材フォルダー/Stage.png");
-	//imgFloor = LoadGraphWithCheck("image/bg1.png");
-	//imgWallL = LoadGraphWithCheck("image/bg2.png");
-	//imgWallR = LoadGraphWithCheck("image/bg3.png");
+	imgSky = LoadGraphWithCheck("repo用素材フォルダー/Sky.png");
+	imgMountain = LoadGraphWithCheck("repo用素材フォルダー/Mountain.png");
+	imgGround = LoadGraphWithCheck("repo用素材フォルダー/Ground.png");
+
 	// 自機と自機の弾の画像の読み込み
 	imgCat = LoadGraphWithCheck("repo用素材フォルダー/Cat.png");
+	imgClaw = LoadGraphWithCheck("repo用素材フォルダー/Claw.png");
+
+	for (int i = 0; i < BADFOOD_KINDNUM; i++)
+	{
+		char fileName[50] = "repo用素材フォルダー/";
+		strcat_s(fileName, BadFoodImageName[i]);
+		strcat_s(fileName, ".png");
+		imgBAD[i] = LoadGraphWithCheck(fileName);
+	}
+
+	for (int i = 0; i < GOODFOOD_KINDNUM; i++)
+	{
+		char fileName[50] = "repo用素材フォルダー/";
+		strcat_s(fileName, GoodFoodImageName[i]);
+		strcat_s(fileName, ".png");
+		imgGOOD[i] = LoadGraphWithCheck(fileName);
+	}
 	//imgBullet = LoadGraphWithCheck("image/bullet.png");
-	//// 敵の画像の読み込み
-	//for (int i = 0; i < IMG_ENEMY_MAX; i++) {
-	//	char file[] = "image/enemy*.png";
-	//	file[11] = (char)('0' + i);
-	//	imgEnemy[i] = LoadGraphWithCheck(file);
-	//}
-	//// その他の画像の読み込み
-	//imgExprosion = LoadGraphWithCheck("image/explosion.png"); // 爆発演出
-	//imgItem = LoadGraphWithCheck("image/item.png"); // アイテム
+	// 敵の画像の読み込み
+	// その他の画像の読み込み
 
 	//// サウンドの読み込みと音量設定
 	//bgm = LoadSoundMemWithCheck("sound/bgm.mp3");
@@ -98,10 +115,14 @@ void InitGame(void) // Initは初期化という意味（initialize）
 
 void ScrollBG(int spd) 
 {
-	static int stageX;
-	stageX = (stageX - spd) % WIDTH;
-	DrawGraph(stageX + WIDTH, 0, imgStage, FALSE);
-	DrawGraph(stageX, 0, imgStage, FALSE);
+	static int GroundX,MountainX,SkyX;
+	SkyX = (SkyX - spd) % WIDTH;
+	DrawGraph(SkyX + WIDTH, 0, imgSky,TRUE);
+	DrawGraph(SkyX, 0, imgSky,TRUE);
+	MountainX = (MountainX - spd * 2) % WIDTH;
+	DrawGraph(MountainX + WIDTH, 0, imgMountain, TRUE);
+	DrawGraph(MountainX, 0, imgMountain,TRUE);
+	DrawGraph(GroundX, 0, imgGround, TRUE);
 }
 
 void InitVariable(void)
@@ -125,23 +146,26 @@ void MovePlayer(void)
 	static int count = 0;
 	ix = player.timer * 94; // 画像の切り出し位置
 
-	if (CheckHitKey(KEY_INPUT_UP)) {
+	if (CheckHitKey(KEY_INPUT_W)) {
 		player.y -= player.vy;
 		if (player.y < HEIGHT / 2 ) { player.y = HEIGHT / 2; }
 	}
-	if (CheckHitKey(KEY_INPUT_DOWN)) {
+	if (CheckHitKey(KEY_INPUT_S)) {
 		player.y += player.vy;
 		if (player.y > HEIGHT - 23) { player.y = HEIGHT - 23; }
 	}
-	if (CheckHitKey(KEY_INPUT_LEFT)) {
+	if (CheckHitKey(KEY_INPUT_A)) {
 		player.x -= player.vx;
 		if (player.x < 47) { player.x = 47; }
 	}
-	if (CheckHitKey(KEY_INPUT_RIGHT)) {
+	if (CheckHitKey(KEY_INPUT_D)) {
 		player.x += player.vx;
-		if (player.x > WIDTH-47) { player.x = WIDTH - 47; }
+		if (player.x > WIDTH - 47) { player.x = WIDTH - 47; }
 	}
-	DrawRectGraph(player.x-47, player.y-46,ix,0,94,92,imgCat,TRUE,FALSE);
+	if (GetMouseInput()&MOUSE_INPUT_LEFT) {
+
+	}
+	DrawRectGraph(player.x - 47, player.y - 46, ix, 0, 94, 92, imgCat, TRUE, FALSE);
 	count++;
 	if (count % 4 == 0) {
 		player.timer++;
